094599b543254980f814dbc44f2a823bc735f576
dmitriieremenko <59915781+dmitriieremenko@users.noreply.github.com>
2020-12-05 19:26:36 +0200

    full_desktop

---
+++ js/global.js
@@ -0,0 +1,4 @@
+document.querySelector ("form").addEventListener("submit", function(e){
+  alert("Submited!");
+  e.preventDefault();
+})
---
+++ js/index.js
@@ -0,0 +1,60 @@
+
+$(document).ready(function() {
+	$('#pagepiling').pagepiling( { 		
+		onLeave: function(index, nextIndex, direction){
+if(index == 1 && direction =='down'){
+	document.querySelector(".header") .classList.add("header__section2")
+	console.log("сработало")
+
+}
+
+if(index == 2 && direction =='up'){
+	document.querySelector(".header") .classList.remove("header__section2")
+	console.log("сработало")
+}
+
+if(index == 2 && direction =='down'){
+	document.querySelector(".header") .classList.remove("header__section2")
+	console.log("сработало")
+
+}
+
+if(index == 3 && direction =='up'){
+	document.querySelector(".header") .classList.add("header__section2")
+	console.log("сработало")
+
+}
+
+
+if(index == 3 && direction =='down'){
+	document.querySelector(".header") .classList.add("header__section2")
+	console.log("сработало")
+
+}
+
+if(index == 4 && direction =='up'){
+	document.querySelector(".header") .classList.remove("header__section2")
+	console.log("сработало")
+
+}
+
+if(index == 4 && direction =='down'){
+	document.querySelector(".header") .classList.remove("header__section2")
+	console.log("сработало")
+
+
+}
+
+if(index == 5 && direction =='up'){
+	document.querySelector(".header") .classList.add("header__section2")
+	console.log("сработало")
+
+
+}
+
+
+	}});
+
+});
+
+
